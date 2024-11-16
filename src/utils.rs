6df047acd90ff07fada079f6f552e4f761794bad
V-Karch <luna.karch05@gmail.com>
2024-11-15 19:19:47 -0500

    utils.rs: removed unused functions

--- src/utils.rs
+++ src/utils.rs
@@ -1,35 +1,6 @@
-use sqlx::{sqlite::{SqlitePool, SqliteQueryResult}, Pool};
+use sqlx::{sqlite::SqlitePool, Pool};
 
 pub async fn connect() -> Result<Pool<sqlx::Sqlite>, sqlx::Error> {
     let pool: Pool<sqlx::Sqlite> = SqlitePool::connect("sqlite:pokemon.db").await?;
     return Ok(pool);
-}
-
-pub async fn create_moves_table(pool: &Pool<sqlx::Sqlite>) -> Result<SqliteQueryResult, sqlx::Error> {
-    let query: SqliteQueryResult = sqlx::query(
-        "CREATE TABLE moves (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name VARCHAR(255) NOT NULL,
-            type VARCHAR(255) NOT NULL,
-            category VARCHAR(255) NOT NULL,
-            power INTEGER,
-            accuracy INTEGER,
-            PP INTEGER,
-            effect TEXT,
-            probability INTEGER
-        );
-        "
-    )
-    .execute(pool)
-    .await?;
-
-    return Ok(query);
-}
-
-pub async fn fill_moves_table(pool: &Pool<sqlx::Sqlite>) -> Result<SqliteQueryResult, sqlx::Error> {
-    let query = sqlx::query("")
-        .execute(pool)
-        .await?;
-
-    return Ok(query);
 }
