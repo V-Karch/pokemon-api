7a4266134f53e9b75b7ed0c1e68e6794b7e20f65
V-Karch <luna.karch05@gmail.com>
2024-11-15 20:51:45 -0500

    added struct for Ability and EVYield, working on the rest

--- src/model.rs
+++ src/model.rs
@@ -10,3 +10,41 @@ pub struct Move {
     pub effect: Option<String>,
     pub probability: Option<i32>,
 }
+
+#[derive(serde::Serialize)]
+pub struct Ability {
+    pub slot: i32,
+    pub name: String,
+    pub effect: String,
+    pub hidden: bool
+}
+
+#[derive(serde::Serialize)]
+pub struct EVYield {
+    pub stat: String,
+    pub amount: i32
+}
+
+#[derive(serde::Serialize)]
+pub struct Pokemon {
+    pub id: i32,
+    pub national_number: i32,
+    pub types: Vec<String>,
+    pub height: f32,
+    pub weight: f32,
+    pub abilities: Vec<Ability>,
+    pub local_numbers: Vec<i32>,
+    pub ev_yield: EVYield
+    // catch rate
+    // base friendship
+    // growth rate
+    // egg groups
+    // gender ratios
+    // egg cycles
+    // base stats
+    // type defenses
+    // evolution chart,
+    // entries
+    // moves
+    // locations
+}
