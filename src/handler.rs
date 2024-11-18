7c4ecd6cfc269b46fb8b277587ec26feba906896
V-Karch <luna.karch05@gmail.com>
2024-11-18 16:23:39 -0500

    added explicit status codes returning the json

--- src/handler.rs
+++ src/handler.rs
@@ -1,41 +1,46 @@
 use crate::model::{self, WebResult};
 
 use sqlx::Row;
-use warp::reply::json;
+use warp::{http::StatusCode, reply::json};
 
 pub async fn version_info() -> model::WebResult<impl warp::Reply> {
     let response_json = &model::VersionInfoResponse {
         status: "Success".to_string(),
         version: "pokemon-api v0.0.1".to_string(),
     };
-    return Ok(json(response_json));
+    Ok(json(response_json))
 }
 
 pub async fn search_move_by_name(
     move_name: String,
     pool: sqlx::SqlitePool,
 ) -> model::WebResult<impl warp::Reply> {
-    if let Ok(row) = &sqlx::query("SELECT * FROM moves WHERE name = ?")
+    match sqlx::query("SELECT * FROM moves WHERE name = ?")
         .bind(move_name.replace(" ", "-"))
         .fetch_one(&pool)
         .await
     {
-        return Ok(json(&model::Move {
-            id: row.get("id"),
-            name: row.get("name"),
-            move_type: row.get("type"),
-            category: row.get("category"),
-            power: row.try_get("power").ok(),
-            accuracy: row.try_get("accuracy").ok(),
-            pp: row.try_get("PP").ok(),
-            effect: row.try_get("effect").ok(),
-            probability: row.try_get("probability").ok(),
-        }));
-    } else {
-        return Ok(json(&model::GenericFailure {
-            status: "Error".to_string(),
-            message: "Failed to retrieve mode".to_string(),
-        }));
+        Ok(row) => Ok(warp::reply::with_status(
+            json(&model::Move {
+                id: row.get("id"),
+                name: row.get("name"),
+                move_type: row.get("type"),
+                category: row.get("category"),
+                power: row.try_get("power").ok(),
+                accuracy: row.try_get("accuracy").ok(),
+                pp: row.try_get("PP").ok(),
+                effect: row.try_get("effect").ok(),
+                probability: row.try_get("probability").ok(),
+            }),
+            StatusCode::OK,
+        )),
+        Err(_) => Ok(warp::reply::with_status(
+            json(&model::GenericFailure {
+                status: "Error".to_string(),
+                message: format!("Move `{}` not found", move_name),
+            }),
+            StatusCode::NOT_FOUND,
+        )),
     }
 }
 
@@ -43,70 +48,89 @@ pub async fn search_move_by_id(
     move_id: i32,
     pool: sqlx::SqlitePool,
 ) -> model::WebResult<impl warp::Reply> {
-    if let Ok(row) = sqlx::query("SELECT * FROM moves WHERE id = ?")
+    match sqlx::query("SELECT * FROM moves WHERE id = ?")
         .bind(move_id)
         .fetch_one(&pool)
         .await
     {
-        return Ok(json(&model::Move {
-            id: row.get("id"),
-            name: row.get("name"),
-            move_type: row.get("type"),
-            category: row.get("category"),
-            power: row.try_get("power").ok(),
-            accuracy: row.try_get("accuracy").ok(),
-            pp: row.try_get("PP").ok(),
-            effect: row.try_get("effect").ok(),
-            probability: row.try_get("probability").ok(),
-        }));
-    } else {
-        return Ok(json(&model::GenericFailure {
-            status: "Error".to_string(),
-            message: "Failed to retrieve move".to_string(),
-        }));
+        Ok(row) => Ok(warp::reply::with_status(
+            json(&model::Move {
+                id: row.get("id"),
+                name: row.get("name"),
+                move_type: row.get("type"),
+                category: row.get("category"),
+                power: row.try_get("power").ok(),
+                accuracy: row.try_get("accuracy").ok(),
+                pp: row.try_get("PP").ok(),
+                effect: row.try_get("effect").ok(),
+                probability: row.try_get("probability").ok(),
+            }),
+            StatusCode::OK,
+        )),
+        Err(_) => Ok(warp::reply::with_status(
+            json(&model::GenericFailure {
+                status: "Error".to_string(),
+                message: format!("Move ID `{}` not found", move_id),
+            }),
+            StatusCode::NOT_FOUND,
+        )),
     }
 }
 
-pub async fn search_ability_by_id(move_id: i32, pool: sqlx::SqlitePool) -> model::WebResult<impl warp::Reply> {
-    if let Ok(row) = &sqlx::query("SELECT * FROM abilities WHERE id = ?")
-        .bind(move_id)
+pub async fn search_ability_by_id(
+    ability_id: i32,
+    pool: sqlx::SqlitePool,
+) -> model::WebResult<impl warp::Reply> {
+    match sqlx::query("SELECT * FROM abilities WHERE id = ?")
+        .bind(ability_id)
         .fetch_one(&pool)
         .await
     {
-        return Ok(json(&model::Ability {
-            id: row.get("id"),
-            name: row.get("name"),
-            pokemon_count: row.get("pokemon"),
-            effect: row.get("effect"),
-            generation: row.get("generation"),
-        }));
-    } else {
-        return Ok(json(&model::GenericFailure {
-            status: "Error".to_string(),
-            message: "Failed to retrieve ability".to_string(),
-        }));
+        Ok(row) => Ok(warp::reply::with_status(
+            json(&model::Ability {
+                id: row.get("id"),
+                name: row.get("name"),
+                pokemon_count: row.get("pokemon"),
+                effect: row.get("effect"),
+                generation: row.get("generation"),
+            }),
+            StatusCode::OK,
+        )),
+        Err(_) => Ok(warp::reply::with_status(
+            json(&model::GenericFailure {
+                status: "Error".to_string(),
+                message: format!("Ability ID `{}` not found", ability_id),
+            }),
+            StatusCode::NOT_FOUND,
+        )),
     }
 }
 
 pub async fn search_ability_by_name(
-    move_name: String, pool: sqlx::SqlitePool
+    ability_name: String,
+    pool: sqlx::SqlitePool,
 ) -> WebResult<impl warp::Reply> {
-    if let Ok(row) = sqlx::query("SELECT * FROM abilities WHERE name = ?")
-        .bind(move_name)
+    match sqlx::query("SELECT * FROM abilities WHERE name = ?")
+        .bind(&ability_name)
         .fetch_one(&pool)
         .await
     {
-        return Ok(json(&model::Ability {
-            id: row.get("id"),
-            name: row.get("name"),
-            pokemon_count: row.get("pokemon"),
-            effect: row.get("effect"),
-            generation: row.get("generation"),
-        }));
-    } else {
-        return Ok(json(&model::GenericFailure {
-            status: "Error".to_string(),
-            message: "Failed to retrieve ability".to_string(),
-        }));
+        Ok(row) => Ok(warp::reply::with_status(
+            json(&model::Ability {
+                id: row.get("id"),
+                name: row.get("name"),
+                pokemon_count: row.get("pokemon"),
+                effect: row.get("effect"),
+                generation: row.get("generation"),
+            }),
+            StatusCode::OK,
+        )),
+        Err(_) => Ok(warp::reply::with_status(
+            json(&model::GenericFailure {
+                status: "Error".to_string(),
+                message: format!("Ability `{}` not found", ability_name),
+            }),
+            StatusCode::NOT_FOUND,
+        )),
     }
 }
