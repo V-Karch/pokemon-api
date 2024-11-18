4efe2ab25c6a28a604ea67db546b05c804a92ddf
V-Karch <luna.karch05@gmail.com>
2024-11-18 13:10:17 -0500

    added a version info checker at the base api url /api/

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod utils;
-mod handler;
 mod model;
+mod handler;
+
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
@@ -16,4 +18,18 @@ async fn main() {
     ).await;
 
     println!("{}", &ability_as_json);
+
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "api=info");
+    }
+    pretty_env_logger::init();
+
+    let version_info_route = warp::path!("api")
+        .and(warp::get())
+        .and_then(handler::version_info);
+
+    let routes = version_info_route.with(warp::log("api"));
+
+    println!("🚀 Server started successfully");
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
