7b1ae9fb3949b763147cf2179977b76bcc8cede4
V-Karch <luna.karch05@gmail.com>
2024-11-18 13:34:46 -0500

    added a temporary planning route for searching by name

--- src/handler.rs
+++ src/handler.rs
@@ -63,6 +63,14 @@ pub async fn search_move_by_id(pool: &sqlx::SqlitePool, move_id: i32) -> Option<
     }
 }
 
+pub async fn search_move_by_id_temp(move_id: i32) -> WebResult<impl Reply> {
+    let response_json = &VersionInfoResponse {
+        status: "Success".to_string(),
+        version: format!("Input: {}", move_id),
+    };
+    return Ok(json(response_json));
+}
+
 pub async fn search_ability_by_id(pool: &sqlx::SqlitePool, move_id: i32) -> Option<model::Ability> {
     if let Ok(row) = sqlx::query("SELECT * FROM abilities WHERE id = ?")
         .bind(move_id)
